cfe415c1e334670346cf590b64dd6c54d112f52e
yeonjun03github <112200769+yeonjun03github@users.noreply.github.com>
2025-05-17 18:40:35 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ComfortCalculator from './ComfortCalculator';
+   
+   function App() {
+     return (
+       <div className="App">
+         <ComfortCalculator />
+       </div>
+     );
+   }
+   
+   export default App;
